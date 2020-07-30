eba3097a81ec1c2eea1c413bf7684e6f44a76e1a
newwon14 <cac2121@daum.net>
2020-07-30 20:13:39 +0900

    최종완성코드

---
+++ src/routes/Detail.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Detail extends React.Component {
+    componentDidMount() {
+        
+    // eslint-disable-next-line
+        const { location, history } = this.props;
+        if(location.state === undefined){
+            history.push("/");
+
+        }
+    }
+
+    render() {
+        const { location } = this.props;
+        if (location.state){
+
+        
+        return <span>{location.state.title}</span>;
+        } else {
+            return null;
+            
+        }
+    }
+
+}
+
+export default Detail;
