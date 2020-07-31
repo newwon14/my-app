99c552cdfb2de15b7c696d9715abb0adf3f71b0f
newwon14 <cac2121@daum.net>
2020-07-31 19:16:21 +0900

    Updates

---
+++ precache-manifest.7e0646501cfc3938df028a813d7e3fad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4544995515586afbc995d1ba16542a46",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "30d6e79f227da6967b7d",
+    "url": "/my-app/static/css/main.a1d63907.chunk.css"
+  },
+  {
+    "revision": "1ad7e30765d57bd3aeb5",
+    "url": "/my-app/static/js/2.648e9191.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.648e9191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d6e79f227da6967b7d",
+    "url": "/my-app/static/js/main.887c0e4c.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
